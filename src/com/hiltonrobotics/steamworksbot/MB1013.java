bcb61ea20b8b1c4a4077f19f357055321b42ad57
robotics <robotics@debian-robotics.hilton-robotics.net>
2018-01-20 13:37:45 -0500

    Pushing to repo

---
+++ src/com/hiltonrobotics/steamworksbot/MB1013.java
@@ -0,0 +1,20 @@
+package com.hiltonrobotics.steamworksbot;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
+public class MB1013 {
+    private AnalogInput in;
+
+    public MB1013(int n) {
+        in = new AnalogInput(n);
+        in.setAverageBits(4);
+    }
+
+    public int distMM() {
+        return in.getAverageValue() / 4 * 5;
+    }
+
+    public AnalogInput getInput() {
+        return in;
+    }
+}
