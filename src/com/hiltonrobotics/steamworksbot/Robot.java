e16086dce612e747b3133301957147e39af65c70
powerboat9 <21oavery@ga.hiltoncsd.net>
2018-03-05 19:00:09 -0500

    Bug fix, commented out some AutoCamManager variable definitions

--- src/com/hiltonrobotics/steamworksbot/Robot.java
+++ src/com/hiltonrobotics/steamworksbot/Robot.java
@@ -35,7 +35,7 @@ public class Robot extends TimedRobot {
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
-	public static AutoCamManager camManager = null;
+	//public static AutoCamManager camManager = null;
 	public static Robot instance;
 	
 	@Override
